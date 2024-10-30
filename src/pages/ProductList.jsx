22647c63fddd25a163daf20a88ae7498be510217
arjun-saseendran <arjunsaseendran@icloud.com>
2024-10-30 06:53:11 +0530

    add ProductList

---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function ProductList() {
+  return (
+    <>
+ <form >
+    <input type="text" placeholder='Search'/>
+    <input type="submit" value='Search'/>
+</form>
+
+<h1>Product List</h1>
+
+    </>
+  )
+}
+
+export default ProductList
