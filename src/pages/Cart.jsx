62f0da998549700e401d91ab2b3735fe9340cf7b
arjun-saseendran <arjunsaseendran@icloud.com>
2024-11-14 22:25:08 +0530

    update cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,69 +1,89 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 function Cart() {
-  const [cartItems, setCartItems] = useState([])
+  const [cartItems, setCartItems] = useState([]);
   const getCartData = () => {
-    const token = localStorage.getItem("token")
-    const config = { headers: {Authorization: token} }
-    axios.get("https://ecommerce-project-backend-one.vercel.app/cart", config).then(res => {
-      setCartItems(res.data.cartItems)
-    }).catch(error => console.log(error.response)
-    )
-  }
+    const token = localStorage.getItem("token");
+    const config = { headers: { Authorization: token } };
+    axios
+      .get("https://ecommerce-project-backend-one.vercel.app/cart", config)
+      .then((res) => {
+        setCartItems(res.data.cartItems);
+      })
+      .catch((error) => console.log(error.response));
+  };
+
+  useEffect(() => {
+    getCartData();
+  }, []);
+
+  const updateCartQunatity = (id, quantity, index) => {
+    const token = localStorage.getItem("token");
+    const config = { headers: { Authorization: token } };
+
+    const data = {
+      quantity,
+      cartItemId: id,
+    };
+
+    axios
+      .post(
+        "https://ecommerce-project-backend-one.vercel.app/cart/update-quantity",
+        data,
+        config
+      )
+      .then(() => {
+        getCartData();
+      })
+      .catch((error) => console.log(error.response));
+  };
   return (
     <>
       <h1>Cart</h1>
       <ul className="cart-list">
-        <li className="cart-item">
-          <img
-            src="https://rukminim2.flixcart.com/image/850/1000/l0igvww0/mobile/r/q/m/-original-imagca5ajerqpfjy.jpeg?q=20&crop=false"
-            alt="iphone13-mobile-phone"
-            width="100px"
-          />
-          <p className="product-title">iPhone 13(128) Green</p>
-          <p className="product-price">₹42999</p>
-          <div>
-            <button>-</button>
-            <span className="cart-item-quantity">3</span>
-            <button>+</button>
-          </div>
-          <p className="product-total">₹128997</p>
-        </li>
-        <li className="cart-item">
-          <img
-            src="https://rukminim2.flixcart.com/image/850/1000/l0igvww0/mobile/r/q/m/-original-imagca5ajerqpfjy.jpeg?q=20&crop=false"
-            alt="iphone13-mobile-phone"
-            width="100px"
-          />
-          <p className="product-title">iPhone 13(128) Green</p>
-          <p className="product-price">₹42999</p>
-          <div>
-            <button>-</button>
-            <span className="cart-item-quantity">3</span>
-            <button>+</button>
-          </div>
-          <p className="product-total">₹128997</p>
-        </li>
-        <li className="cart-item">
-          <img
-            src="https://rukminim2.flixcart.com/image/850/1000/l0igvww0/mobile/r/q/m/-original-imagca5ajerqpfjy.jpeg?q=20&crop=false"
-            alt="iphone13-mobile-phone"
-            width="100px"
-          />
-          <p className="product-title">iPhone 13(128) Green</p>
-          <p className="product-price">₹42999</p>
-          <div>
-            <button>-</button>
-            <span className="cart-item-quantity">3</span>
-            <button>+</button>
-          </div>
-          <p className="product-total">₹128997</p>
-        </li>
+        {cartItems.map((cartItem, index) => (
+          <li className="cart-item">
+            <img src={cartItem.product.image} width="100px" />
+            <p className="product-title">{cartItem.product.title}</p>
+            <p className="product-title">{cartItem.product.description}</p>
+            <p className="product-price">{cartItem.product.price}</p>
+            <div>
+              <button
+                onClick={() => {
+                  updateCartQunatity(
+                    cartItem._id,
+                    cartItem.quantity - 1,
+                    index
+                  );
+                }}
+              >
+                -
+              </button>
+              <span className="cart-item-quantity">{cartItem.quantity}</span>
+              <button
+                onClick={() => {
+                  updateCartQunatity(
+                    cartItem._id,
+                    cartItem.quantity + 1,
+                    index
+                  );
+                }}
+              >
+                +
+              </button>
+            </div>
+            <p className="product-total">
+              ${cartItem.price * cartItem.quantity}
+            </p>
+          </li>
+        ))}
       </ul>
       <p className="text-right">Total price: ₹42999</p>
-      <Link className="btn2" to='/checkout'>Checkout</Link>
+      <Link className="btn2" to="/checkout">
+        Checkout
+      </Link>
     </>
   );
 }
