c18639b429887efc7770b20d159d614d4c4ce8d1
arjun-saseendran <arjunsaseendran@icloud.com>
2024-11-14 06:38:06 +0530

    root

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,22 +1,23 @@
-import React from 'react'
-import { Outlet, Link } from 'react-router-dom'
-
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
 
 function Root() {
   return (
-    <div className='container'>
-    <header>
+    <div className="container">
+      <header>
         <h1>Header</h1>
-        <Link to='/login'>Login</Link>
-    </header>
-    <main>
-        <Outlet/>
-    </main>
-    <footer>
+        <Link to="/login" style={{ textDecoration: "none" }}>
+          Login
+        </Link>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <footer>
         <h1>Footer</h1>
-    </footer>
+      </footer>
     </div>
-  )
+  );
 }
 
-export default Root
+export default Root;
