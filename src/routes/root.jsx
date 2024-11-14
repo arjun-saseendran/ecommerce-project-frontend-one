ec2a8cc0ce423a577b38743f6ee1f09a9b46d175
arjun-saseendran <arjunsaseendran@icloud.com>
2024-11-14 22:28:39 +0530

    add link style

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -6,7 +6,12 @@ function Root() {
     <div className="container">
       <header>
         <h1>Header</h1>
-        <Link to="/login">Login</Link>
+        <div>
+          <Link className="link-style" to="/cart">
+            Cart(2)
+          </Link >
+          <Link className="link-style" to="/login">Login</Link>
+        </div>
       </header>
       <main>
         <Outlet />
